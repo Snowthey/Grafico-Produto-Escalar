01aeb5b9aa7d73b90b3ce4470033aa00b4764e51
Matheus <matheus.garcez09@gmail.com>
2024-05-08 15:26:21 -0300

    Grafico funcionando para ambos algoritimos

--- src/ProdutoEscalarComplexidadeReduzida.java
+++ src/ProdutoEscalarComplexidadeReduzida.java
@@ -1,18 +1,17 @@
 public class ProdutoEscalarComplexidadeReduzida {
 
-    public static int calcularProdutoEscalar(int[] vetor1, int[] vetor2) {
+    public static long calcularProdutoEscalar(int[] vetor1, int[] vetor2) throws IllegalArgumentException {
         if (vetor1.length != vetor2.length) {
-            System.out.println("Os vetores devem ter o mesmo tamanho");
-            return -1;
+            throw new IllegalArgumentException("Os vetores devem ter o mesmo tamanho");
         }
 
-
-        int resultado = 0;
+        long resultado = 0;
 
         for (int i = 0; i < vetor1.length; i++) {
-            resultado += vetor1[i] * vetor2[i];
+            resultado += (long) vetor1[i] * vetor2[i];
         }
 
         return resultado;
     }
+
 }
